d95437f38efa69fe1e720093943ff3216a0c6cc2
Andrew Premdas <apremdas@yahoo.co.uk>
2011-09-09 18:07:42 +0100

    ran  "rails new . -m http://jruby.org/"

---
+++ rails-example/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsExample::Application.config.secret_token = '11dfacd2032d1cf24c39219e8df78b64bd206cabee6b0303c889a80af5d162c0ced73a2bc635bfd0a9dcdd836153d682bc4858bff1f63c62e463a67558c75776'
